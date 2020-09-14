a234aa91a56f068bff788c1eadc63d7b29c4dd5b
joycehsu <m234onica@gmail.com>
2020-09-14 14:40:38 +0800

    rename col

--- Agg_chemical.py
+++ Agg_chemical.py
@@ -6,10 +6,9 @@ import pandas as pd
 csv_file = os.path.join('./result', "filter_chemical.csv")
 data = pd.read_csv(csv_file)
 
-output = pd.DataFrame(columns=["公司地址", "count"])
-
+output = pd.DataFrame()
 area = data["公司地址"].str.slice(0, 3)
-output = data["公司名稱"].groupby(area).count()
+output["Count"] = data.groupby(area)["公司名稱"].count()
 
 print(output)
 
