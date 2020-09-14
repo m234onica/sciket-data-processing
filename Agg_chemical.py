d8de7e68125872f7d7d757c6be93ab3689301293
joycehsu <m234onica@gmail.com>
2020-09-14 15:35:13 +0800

    add group by "capital" in Agg_chemical.csv

--- Agg_chemical.py
+++ Agg_chemical.py
@@ -6,11 +6,17 @@ import pandas as pd
 csv_file = os.path.join('./result', "filter_chemical.csv")
 data = pd.read_csv(csv_file)
 
-output = pd.DataFrame()
-area = data["公司地址"].str.slice(0, 3)
-output["Count"] = data.groupby(area)["公司名稱"].count()
+output_address = pd.DataFrame()
+address = data["公司地址"].str.slice(0, 3)
+output_address["Count"] = data.groupby(address)["公司名稱"].count()
 
-print(output)
+output_capital = pd.DataFrame()
+bins = [x * 1000000 for x in range(100000)]
+capital = pd.cut(data["實收資本額"], bins, right=False)
+
+output_capital["Count"] = data["公司名稱"].groupby(capital).count()
 
 Result = os.path.join("./result", "Agg_chemical.csv")
-output.to_csv(Result)
+result = pd.concat([output_address, output_capital])
+
+result.to_csv(Result)
