582ca2ec3688839043d30ddbe3a22205729d6ec9
joycehsu <m234onica@gmail.com>
2020-09-14 12:14:49 +0800

    add secon fiter and drop duplicate datas

--- filter_chemical.py
+++ filter_chemical.py
@@ -11,8 +11,10 @@ result = pd.DataFrame()
 
 filter_id = ["181", "183", "184", "185", "199", "200", "210"]
 for id in filter_id:
-    output = data[data['行業代號（財政資訊中心匯入）'].str.match(id)]
+    output = data[data['行業代號（財政資訊中心匯入）'].str.match(id) |
+                  data['行業代號（財政資訊中心匯入）'].str.contains("," + id)]
     result = result.append(output)
 
-result = result["公司名稱"].to_csv(Result, index=False)
+uni_result = result.drop_duplicates(subset=["公司名稱"], keep="first")
+uni_result.to_csv(Result, index=False)
 print("success: ", pd.read_csv(Result).shape)
